14cfd22937166708f4a15f8bb657765fe1d3e328
Mateus <mateusantonioofc@gmail.com>
2024-01-26 17:42:21 -0300

    feat: create Product Record DTO

---
+++ src/main/java/com/github/mateusantonioofc/apirest/dtos/ProductRecordDto.java
@@ -0,0 +1,8 @@
+package com.github.mateusantonioofc.apirest.dtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) { }
