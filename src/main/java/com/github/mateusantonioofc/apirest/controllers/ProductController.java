ac44d42505f676b576d66d6ce3d904917db93d6f
Mateus <mateusantonioofc@gmail.com>
2024-01-28 00:33:58 -0300

    feat: create Get product By Id endpoint

--- src/main/java/com/github/mateusantonioofc/apirest/controllers/ProductController.java
+++ src/main/java/com/github/mateusantonioofc/apirest/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import com.github.mateusantonioofc.apirest.repositories.ProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -28,13 +31,25 @@ public class ProductController {
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED)
             .body(productRepository.save(productModel));
-        
     }
 
     @GetMapping
     public ResponseEntity<List<ProductModel>> getAllProducts() {
         return ResponseEntity.status(HttpStatus.OK)
             .body(productRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProductsById(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> productFind = productRepository.findById(id);
+
+        if (productFind.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Product not found");
+        }
+        
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(productFind.get());
     }
     
 }
