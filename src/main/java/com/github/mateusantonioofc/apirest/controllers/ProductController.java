f6b792685df39e071b166b9208df08a289341442
Mateus <mateusantonioofc@gmail.com>
2024-01-27 22:53:43 -0300

    feat: create Save Products endpoint

--- src/main/java/com/github/mateusantonioofc/apirest/controllers/ProductController.java
+++ src/main/java/com/github/mateusantonioofc/apirest/controllers/ProductController.java
@@ -1,11 +1,9 @@
 package com.github.mateusantonioofc.apirest.controllers;
 
-import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +11,26 @@ import com.github.mateusantonioofc.apirest.dtos.ProductRecordDto;
 import com.github.mateusantonioofc.apirest.models.ProductModel;
 import com.github.mateusantonioofc.apirest.repositories.ProductRepository;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @RestController
-
+@RequestMapping("/products")
 public class ProductController {
     
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping()
+    public ResponseEntity<ProductModel> saveProducts(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        ProductModel productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(productRepository.save(productModel));
+        
+    }
+
     
     
 }
