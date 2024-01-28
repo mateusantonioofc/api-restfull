e1191f070aa7735e2c420096697312dbdc6c4a1a
Mateus <mateusantonioofc@gmail.com>
2024-01-27 22:51:21 -0300

    feat: create Product Controller

---
+++ src/main/java/com/github/mateusantonioofc/apirest/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.github.mateusantonioofc.apirest.controllers;
+
+import java.util.List;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.github.mateusantonioofc.apirest.dtos.ProductRecordDto;
+import com.github.mateusantonioofc.apirest.models.ProductModel;
+import com.github.mateusantonioofc.apirest.repositories.ProductRepository;
+import jakarta.validation.Valid;
+
+
+@RestController
+
+public class ProductController {
+    
+    
+    
+}
